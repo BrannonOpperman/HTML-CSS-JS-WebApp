ea6dd42e8492701b158d094ee04a341b36e472d3
Brannon <57905614+BrannonOpperman@users.noreply.github.com>
2019-11-29 00:09:59 +0200

    First Progress Deliverable

---
+++ script.js
@@ -0,0 +1,399 @@
+// AUTHOR DATE WEBSITE
+//DESCRIPTION
+
+$(".first").click(onFirstCircle);
+$(".second").click(onSecondCircle);
+$(".third").click(onThirdCircle);
+$(".fourth").click(onFourthCircle);
+$(".fifth").click(onFifthCircle);
+$(".sixth").click(onSixthCircle);
+$(".seventh").click(onSeventhCircle);
+$(".eighth").click(onEighthCircle);
+$(".nineth").click(onNinethCircle);
+$(".tenth").click(onTenthCircle);
+$(".eleventh").click(onEleventhCircle);
+$(".twelfth").click(onTwelfthCircle);
+$(".thirtenth").click(onThirteenthCircle);
+$(".fourteenth").click(onFourteenthCircle);
+$(".fifteenth").click(onFifteenthCircle);
+$(".sixteenth").click(onSixteenthCircle);
+$(".seventeenth").click(onSeventeenthCircle);
+$(".eighteenth").click(onEighteenthCircle);
+
+function onFirstCircle(){
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".first").toggleClass("clicked");
+}
+
+function onSecondCircle(){
+    $(".first").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".second").toggleClass("clicked");
+}
+
+function onThirdCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".third").toggleClass("clicked");
+}
+
+function onFourthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".fourth").toggleClass("clicked");
+}
+
+function onFifthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".fifth").toggleClass("clicked");
+}
+
+function onSixthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".sixth").toggleClass("clicked");
+}
+
+function onSeventhCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".seventh").toggleClass("clicked");
+}
+
+function onEighthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".eighth").toggleClass("clicked");
+}
+
+function onNinethCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".nineth").toggleClass("clicked");
+}
+
+function onTenthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".tenth").toggleClass("clicked");
+}
+
+function onEleventhCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".eleventh").toggleClass("clicked");
+}
+
+function onTwelfthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".twelfth").toggleClass("clicked");
+}
+
+function onThirteenthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".thirteenth").toggleClass("clicked");
+}
+
+function onFourteenthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".fourteenth").toggleClass("clicked");
+}
+
+function onFifteenthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".fifteenth").toggleClass("clicked");
+}
+
+function onSixteenthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".sixteenth").toggleClass("clicked");
+}
+
+function onSeventeenthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".eighteenth").removeClass("clicked");
+    $(".seventeenth").toggleClass("clicked");
+}
+
+function onEighteenthCircle(){
+    $(".first").removeClass("clicked");
+    $(".second").removeClass("clicked");
+    $(".third").removeClass("clicked");
+    $(".fourth").removeClass("clicked");
+    $(".fifth").removeClass("clicked");
+    $(".sixth").removeClass("clicked");
+    $(".seventh").removeClass("clicked");
+    $(".eighth").removeClass("clicked");
+    $(".nineth").removeClass("clicked");
+    $(".tenth").removeClass("clicked");
+    $(".eleventh").removeClass("clicked");
+    $(".twelfth").removeClass("clicked");
+    $(".thirteenth").removeClass("clicked");
+    $(".fourteenth").removeClass("clicked");
+    $(".fifteenth").removeClass("clicked");
+    $(".sixteenth").removeClass("clicked");
+    $(".seventeenth").removeClass("clicked");
+    $(".eighteenth").toggleClass("clicked");
+}
